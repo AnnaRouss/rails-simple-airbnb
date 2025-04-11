262ad8e73cc1ac5e9ee7cec5cae1e17f95f933d1
AnastasiaRoussakis <a_roussakis@outlook.com>
2025-04-11 08:45:21 -0400

    Completed AirBnb app

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,12 @@
 class FlatsController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     if params[:query].present?
-      @flats = Flat.where('name ILIKE ?', "%#{params[:query]}%")
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?", "%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
     else
       @flats = Flat.all
     end
@@ -19,9 +22,9 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     if @flat.save
-      redirect_to flat_path(@flat)
+      redirect_to flats_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -29,21 +32,23 @@ class FlatsController < ApplicationController
   end
 
   def update
-    @flat = Flat.find(params[:id])
     if @flat.update(flat_params)
       redirect_to flat_path(@flat)
     else
-      render :edit
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @flat = Flat.find(params[:id])
     @flat.destroy
-    redirect_to flats_path
+    redirect_to flats_path, status: :see_other
   end
 
   private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
